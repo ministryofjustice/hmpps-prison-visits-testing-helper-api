2a2b2b825d552d804bf0b6bdee9cb18d57cc8823
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-12-18 10:56:18 +0000

    VB-3311 Fixed up code error to correct http call

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonvisitstestinghelperapi/dto/VisitStatus.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonvisitstestinghelperapi/dto/VisitStatus.kt
@@ -1,11 +1,9 @@
 package uk.gov.justice.digital.hmpps.hmppsprisonvisitstestinghelperapi.dto
 
 @Suppress("unused")
-enum class VisitStatus(
-  val description: String,
-) {
-  RESERVED("Reserved"),
-  CHANGING("Changing"),
-  BOOKED("Booked"),
-  CANCELLED("Cancelled"),
+enum class VisitStatus{
+  RESERVED,
+  CHANGING,
+  BOOKED,
+  CANCELLED,
 }
