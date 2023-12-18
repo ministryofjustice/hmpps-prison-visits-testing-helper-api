73508bd748253a58e4b529b754561cda8d75ad41
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-12-18 10:52:57 +0000

    VB-3311 Fixed up code error to correct http call

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonvisitstestinghelperapi/controller/TestingDBApiBHelperController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonvisitstestinghelperapi/controller/TestingDBApiBHelperController.kt
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.HttpStatus.OK
 import org.springframework.http.MediaType
@@ -13,7 +12,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsprisonvisitstestinghelperapi.dto.VisitStatus
 import uk.gov.justice.digital.hmpps.hmppsprisonvisitstestinghelperapi.repository.VisitRepository
