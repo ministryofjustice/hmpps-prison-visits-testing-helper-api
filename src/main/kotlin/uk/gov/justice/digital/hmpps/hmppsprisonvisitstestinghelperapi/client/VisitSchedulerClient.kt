011a9715a9e3440862c40e1e2ad1d8d21e298d4d
Dhiraj Shetty <110536693+dhirajshettymoj@users.noreply.github.com>
2024-03-01 14:27:06 +0000

    Vb 3544 create a Test controller for Admin API calls to add and remove exclude dates. (#21)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonvisitstestinghelperapi/client/VisitSchedulerClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonvisitstestinghelperapi/client/VisitSchedulerClient.kt
@@ -5,10 +5,11 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.digital.hmpps.hmppsprisonvisitstestinghelperapi.dto.PrisonExcludeDateDto
 import java.time.Duration
-
-const val VISIT_CONTROLLER_PATH: String = "/visits"
+import java.time.LocalDate
 
 @Component
 class VisitSchedulerClient(
@@ -18,5 +19,31 @@ class VisitSchedulerClient(
 
   companion object {
     val LOG: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
+  fun addExcludeDate(prisonCode: String, excludeDate: LocalDate) {
+    LOG.info("Calling add exclude date for prison - $prisonCode, excluded date - $excludeDate")
+    webClient.put()
+      .uri("/admin/prisons/prison/$prisonCode/exclude-date/add")
+      .body(BodyInserters.fromValue(PrisonExcludeDateDto(excludeDate)))
+      .retrieve()
+      .toBodilessEntity()
+      .doOnError { e -> LOG.error("Could not add exclude date :", e) }
+      .block(apiTimeout)
+    LOG.info("Finished calling addExcludeDate for prison - $prisonCode, excluded date - $excludeDate")
+  }
+
+  fun removeExcludeDate(prisonCode: String, excludeDate: LocalDate) {
+    LOG.info("Calling remove exclude date for prison - $prisonCode, excluded date - $excludeDate")
+
+    webClient.put()
+      .uri("/admin/prisons/prison/$prisonCode/exclude-date/remove")
+      .body(BodyInserters.fromValue(PrisonExcludeDateDto(excludeDate)))
+      .retrieve()
+      .toBodilessEntity()
+      .doOnError { e -> LOG.error("Could not remove exclude date :", e) }
+      .block(apiTimeout)
+
+    LOG.info("Finished calling remove exclude date for prison - $prisonCode, excluded date - $excludeDate")
   }
 }
