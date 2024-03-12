898c7430a9c860570f6277298b0a44409da41e9e
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2024-03-12 10:09:21 +0000

    Update dependencies to fix security failures

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,11 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.1")
-  implementation("com.amazonaws:aws-java-sdk-sts:1.12.675")
+  implementation("com.amazonaws:aws-java-sdk-sts:1.12.677")
   implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.6.RELEASE")
-  implementation("com.amazonaws:aws-java-sdk-s3:1.12.675")
+  implementation("com.amazonaws:aws-java-sdk-s3:1.12.677")
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.2")
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.1.0")
 
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.3.0")
