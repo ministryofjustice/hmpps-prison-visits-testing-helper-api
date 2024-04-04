0d4c4092262f8f971a53548fe210b6e7a85774d8
Dhiraj Shetty <110536693+dhirajshettymoj@users.noreply.github.com>
2024-04-04 12:44:01 +0100

    Update dependencies to fix security failures (#25)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.3"
-  kotlin("plugin.spring") version "1.9.22"
-  id("org.jetbrains.kotlin.plugin.noarg") version "1.9.22"
-  kotlin("plugin.jpa") version "1.9.22"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.5"
+  kotlin("plugin.spring") version "1.9.23"
+  id("org.jetbrains.kotlin.plugin.noarg") version "1.9.23"
+  kotlin("plugin.jpa") version "1.9.23"
 }
 
 configurations {
@@ -20,20 +20,20 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.1")
-  implementation("com.amazonaws:aws-java-sdk-sts:1.12.677")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.2")
+  implementation("com.amazonaws:aws-java-sdk-sts:1.12.694")
   implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.6.RELEASE")
-  implementation("com.amazonaws:aws-java-sdk-s3:1.12.677")
+  implementation("com.amazonaws:aws-java-sdk-s3:1.12.694")
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.2")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.1.0")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.2.0")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.3.0")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
-  implementation("org.springdoc:springdoc-openapi-starter-common:2.3.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.5.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+  implementation("org.springdoc:springdoc-openapi-starter-common:2.5.0")
 
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  runtimeOnly("org.postgresql:postgresql:42.7.2")
+  runtimeOnly("org.postgresql:postgresql:42.7.3")
 
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 }
