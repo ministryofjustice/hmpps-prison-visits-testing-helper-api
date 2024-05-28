00c85e670fdcf537effc0d56a3b5fe88505c13ba
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2024-05-28 10:46:10 +0100

    VB-3910 2 Errors on Automated tests, fixed up problem when notifications are not being cleared

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonvisitstestinghelperapi/integration/DBApiHelperControllerIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonvisitstestinghelperapi/integration/DBApiHelperControllerIntegrationTest.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppsprisonvisitstestinghelperapi.integration
 
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
