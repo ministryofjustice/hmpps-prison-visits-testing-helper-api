dcc8aa5886891750fd474a0314e988cd81d85738
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2025-01-08 11:09:15 +0000

    Update testing-helper-api to remove use of old lib
    Move to use the newer aws start libs provided by the hmpps-sqs-starter import

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,6 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.2.2")
   implementation("com.amazonaws:aws-java-sdk-sts:1.12.780")
-  implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.6.RELEASE")
   implementation("com.amazonaws:aws-java-sdk-s3:1.12.780")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonvisitstestinghelperapi/config/LocalstackConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonvisitstestinghelperapi/config/LocalstackConfig.kt
@@ -1,15 +1,14 @@
 package uk.gov.justice.digital.hmpps.hmppsprisonvisitstestinghelperapi.config
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider
-import com.amazonaws.auth.AnonymousAWSCredentials
-import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
-import com.amazonaws.services.sqs.AmazonSQSAsync
-import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
+import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.sqs.SqsAsyncClient
+import java.net.URI
 
 @Configuration
 class LocalstackConfig {
@@ -19,11 +18,11 @@ class LocalstackConfig {
   fun awsSqsClient(
     @Value("\${hmpps.sqs.queues.prisonvisitsevents.endpoint.url}") serviceEndpoint: String?,
     @Value("\${hmpps.sqs.queues.prisonvisitsevents.endpoint.region}") region: String?,
-  ): AmazonSQSAsync {
-    val creds = AnonymousAWSCredentials()
-    return AmazonSQSAsyncClientBuilder.standard()
-      .withCredentials(AWSStaticCredentialsProvider(creds))
-      .withEndpointConfiguration(EndpointConfiguration(serviceEndpoint, region))
+  ): SqsAsyncClient {
+    return SqsAsyncClient.builder()
+      .endpointOverride(URI.create(serviceEndpoint!!))
+      .region(Region.of(region))
+      .credentialsProvider(AnonymousCredentialsProvider.create())
       .build()
   }
 }
