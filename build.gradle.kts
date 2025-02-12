85da3266f2aee19fee5230ad870b2acdeffb87eb
Aaron Davies <168211160+adaviesMOJ@users.noreply.github.com>
2025-02-12 14:54:53 +0000

    Update to latest sqs starter (#146)

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.3.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.3.1")
   implementation("com.amazonaws:aws-java-sdk-sts:1.12.780")
   implementation("com.amazonaws:aws-java-sdk-s3:1.12.780")
 
