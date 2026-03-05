78ea11e4087e16a6ec221f0c3ebe88a1ecad992e
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2026-03-05 12:39:43 +0000

    upgrade sqs lib to v7

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonvisitstestinghelperapi/service/EventHandlerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonvisitstestinghelperapi/service/EventHandlerService.kt
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import tools.jackson.databind.ObjectMapper
 import uk.gov.justice.digital.hmpps.hmppsprisonvisitstestinghelperapi.dto.DomainEvent
+import uk.gov.justice.digital.hmpps.hmppsprisonvisitstestinghelperapi.dto.MessageAttribute
+import uk.gov.justice.digital.hmpps.hmppsprisonvisitstestinghelperapi.dto.MessageAttributes
 import uk.gov.justice.digital.hmpps.hmppsprisonvisitstestinghelperapi.dto.NonAssociationEventDto
 import uk.gov.justice.digital.hmpps.hmppsprisonvisitstestinghelperapi.dto.PrisonerAlertCreatedUpdatedEventDto
 import uk.gov.justice.digital.hmpps.hmppsprisonvisitstestinghelperapi.dto.PrisonerReceivedEventDto
@@ -28,38 +30,51 @@ class EventHandlerService(
 
   fun handleNonAssociationEvent(nonAssociationEventDto: NonAssociationEventDto) {
     LOG.info("Received non association event with details - $nonAssociationEventDto")
+
     val values = mutableMapOf<String, String>()
     values["nsPrisonerNumber1"] = nonAssociationEventDto.prisonerCode
     values["nsPrisonerNumber2"] = nonAssociationEventDto.nonAssociationPrisonerCode
 
-    sqsService.sendDomainEvent(SQSMessage(NOTIFICATION_TYPE, objectMapper.writeValueAsString(DomainEvent(Events.NON_ASSOCIATION_CREATE_EVENT.eventType, values)), UUID.randomUUID().toString()))
+    val domainEventJson = objectMapper.writeValueAsString(DomainEvent(Events.NON_ASSOCIATION_CREATE_EVENT.eventType, values))
+
+    sqsService.sendDomainEvent(buildSqsMessage(Events.NON_ASSOCIATION_CREATE_EVENT.eventType, domainEventJson))
+
     LOG.info("processed non association event with details - $nonAssociationEventDto")
   }
 
   fun handlePrisonerReleaseEvent(prisonerReleasedNotificationDto: PrisonerReleasedNotificationDto) {
     LOG.info("Received prisoner release event with details - $prisonerReleasedNotificationDto")
+
     val values = mutableMapOf<String, String>()
     values["prisonId"] = prisonerReleasedNotificationDto.prisonCode
     values["nomsNumber"] = prisonerReleasedNotificationDto.prisonerCode
     values["reason"] = prisonerReleasedNotificationDto.reason
 
-    sqsService.sendDomainEvent(SQSMessage(NOTIFICATION_TYPE, objectMapper.writeValueAsString(DomainEvent(Events.PRISONER_RELEASE_EVENT.eventType, values)), UUID.randomUUID().toString()))
+    val domainEventJson = objectMapper.writeValueAsString(DomainEvent(Events.PRISONER_RELEASE_EVENT.eventType, values))
+
+    sqsService.sendDomainEvent(buildSqsMessage(Events.PRISONER_RELEASE_EVENT.eventType, domainEventJson))
+
     LOG.info("processed prisoner release event with details - $prisonerReleasedNotificationDto")
   }
 
   fun handlePrisonerReceivedEvent(prisonerEventDto: PrisonerReceivedEventDto) {
     LOG.info("Received prisoner receive event with details - $prisonerEventDto")
+
     val values = mutableMapOf<String, String>()
     values["prisonId"] = prisonerEventDto.prisonCode
     values["nomsNumber"] = prisonerEventDto.prisonerCode
     values["reason"] = prisonerEventDto.reason
 
-    sqsService.sendDomainEvent(SQSMessage(NOTIFICATION_TYPE, objectMapper.writeValueAsString(DomainEvent(Events.PRISONER_RECEIVE_EVENT.eventType, values)), UUID.randomUUID().toString()))
+    val domainEventJson = objectMapper.writeValueAsString(DomainEvent(Events.PRISONER_RECEIVE_EVENT.eventType, values))
+
+    sqsService.sendDomainEvent(buildSqsMessage(Events.PRISONER_RECEIVE_EVENT.eventType, domainEventJson))
+
     LOG.info("processed prisoner receive event with details - $prisonerEventDto")
   }
 
   fun handlePrisonerRestrictionChangeEvent(prisonerRestrictionEventDto: PrisonerRestrictionEventDto) {
     LOG.info("Received prisoner restriction change event with details - $prisonerRestrictionEventDto")
+
     val values = mutableMapOf<String, String>()
     values["nomsNumber"] = prisonerRestrictionEventDto.prisonerCode
     values["effectiveDate"] = prisonerRestrictionEventDto.startDate.toString()
@@ -67,7 +82,10 @@ class EventHandlerService(
       values["expiryDate"] = it.toString()
     }
 
-    sqsService.sendDomainEvent(SQSMessage(NOTIFICATION_TYPE, objectMapper.writeValueAsString(DomainEvent(Events.PRISONER_RESTRICTION_CHANGE_EVENT.eventType, values)), UUID.randomUUID().toString()))
+    val domainEventJson = objectMapper.writeValueAsString(DomainEvent(Events.PRISONER_RESTRICTION_CHANGE_EVENT.eventType, values))
+
+    sqsService.sendDomainEvent(buildSqsMessage(Events.PRISONER_RESTRICTION_CHANGE_EVENT.eventType, domainEventJson))
+
     LOG.info("processed prisoner restriction change event with details - $prisonerRestrictionEventDto")
   }
 
@@ -80,12 +98,16 @@ class EventHandlerService(
     values["alertsAdded"] = prisonerAlertCreatedUpdatedEventDto.alertsAdded
     values["alertsRemoved"] = prisonerAlertCreatedUpdatedEventDto.alertsRemoved
 
-    sqsService.sendDomainEvent(SQSMessage(NOTIFICATION_TYPE, objectMapper.writeValueAsString(DomainEvent(Events.PRISONER_ALERT_UPDATED_EVENT.eventType, values)), UUID.randomUUID().toString()))
+    val domainEventJson = objectMapper.writeValueAsString(DomainEvent(Events.PRISONER_ALERT_UPDATED_EVENT.eventType, values))
+
+    sqsService.sendDomainEvent(buildSqsMessage(Events.PRISONER_ALERT_UPDATED_EVENT.eventType, domainEventJson))
+
     LOG.info("processed prisoner alert updated event with details - $prisonerAlertCreatedUpdatedEventDto")
   }
 
   fun handleVisitorRestrictionChangeEvent(visitorRestrictionEventDto: VisitorRestrictionEventDto) {
     LOG.info("Received visitor restriction change event with details - $visitorRestrictionEventDto")
+
     val values = mutableMapOf<String, String>()
     values["personId"] = visitorRestrictionEventDto.visitorId
     values["effectiveDate"] = visitorRestrictionEventDto.startDate.toString()
@@ -93,7 +115,22 @@ class EventHandlerService(
       values["expiryDate"] = it.toString()
     }
 
-    sqsService.sendDomainEvent(SQSMessage(NOTIFICATION_TYPE, objectMapper.writeValueAsString(DomainEvent(Events.VISITOR_RESTRICTION_CHANGE_EVENT.eventType, values)), UUID.randomUUID().toString()))
+    val domainEventJson = objectMapper.writeValueAsString(DomainEvent(Events.VISITOR_RESTRICTION_CHANGE_EVENT.eventType, values))
+
+    sqsService.sendDomainEvent(buildSqsMessage(Events.VISITOR_RESTRICTION_CHANGE_EVENT.eventType, domainEventJson))
+
     LOG.info("processed visitor restriction change event with details - $visitorRestrictionEventDto")
+  }
+
+  private fun buildSqsMessage(eventType: String, domainEventJson: String): SQSMessage {
+    val attrs = MessageAttributes().apply {
+      this["eventType"] = MessageAttribute(type = "String", value = eventType)
+    }
+
+    return SQSMessage(
+      message = domainEventJson,
+      messageId = UUID.randomUUID().toString(),
+      messageAttributes = attrs,
+    )
   }
 }
