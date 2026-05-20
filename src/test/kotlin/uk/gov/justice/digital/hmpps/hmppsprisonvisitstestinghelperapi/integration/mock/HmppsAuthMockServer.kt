109f9617b90f5f0782f5f3a98e4dad52fa71abfb
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2026-05-20 15:39:51 +0100

    Update dependencies and add .snyk file

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonvisitstestinghelperapi/integration/mock/HmppsAuthMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonvisitstestinghelperapi/integration/mock/HmppsAuthMockServer.kt
@@ -1,15 +1,16 @@
 package uk.gov.justice.digital.hmpps.hmppsprisonvisitstestinghelperapi.integration.mock
 
 import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder
 import com.github.tomakehurst.wiremock.client.WireMock
-import com.github.tomakehurst.wiremock.client.WireMock.aResponse
 import com.github.tomakehurst.wiremock.client.WireMock.post
-import com.github.tomakehurst.wiremock.http.HttpHeader
-import com.github.tomakehurst.wiremock.http.HttpHeaders
+import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
 import org.junit.jupiter.api.extension.AfterAllCallback
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.BeforeEachCallback
 import org.junit.jupiter.api.extension.ExtensionContext
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 
 class HmppsAuthExtension :
   BeforeAllCallback,
@@ -23,11 +24,11 @@ class HmppsAuthExtension :
 
   override fun beforeAll(context: ExtensionContext) {
     hmppsAuthApi.start()
-    hmppsAuthApi.stubGrantToken()
   }
 
   override fun beforeEach(context: ExtensionContext) {
-    hmppsAuthApi.resetRequests()
+    hmppsAuthApi.resetAll()
+    hmppsAuthApi.stubGrantToken()
   }
 
   override fun afterAll(context: ExtensionContext) {
@@ -41,11 +42,13 @@ class HmppsAuthMockServer : WireMockServer(WIREMOCK_PORT) {
   }
 
   fun stubGrantToken() {
+    val responseBuilder = createJsonResponseBuilder()
+
     stubFor(
-      post(WireMock.urlEqualTo("/auth/oauth/token"))
+      post(urlEqualTo("/auth/oauth/token"))
         .willReturn(
-          aResponse()
-            .withHeaders(HttpHeaders(HttpHeader("Content-Type", "application/json")))
+          responseBuilder
+            .withStatus(HttpStatus.OK.value())
             .withBody(
               """
               {
@@ -57,4 +60,6 @@ class HmppsAuthMockServer : WireMockServer(WIREMOCK_PORT) {
         ),
     )
   }
+
+  fun createJsonResponseBuilder(): ResponseDefinitionBuilder = WireMock.aResponse().withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE).withHeader("Connection", "close")
 }
